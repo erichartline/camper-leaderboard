b96a6f622798e4b787db21ab9a956c31c3c42dac
wildlifehexagon <crossthebreeze@gmail.com>
2017-10-21 17:46:50 -0500

    add more tests

--- src/containers/TableListItem.js
+++ src/containers/TableListItem.js
@@ -1,21 +1,29 @@
 import React from 'react';
 
-const TableListItem = ({ user, number }) => {
+export const TableListItem = ({ user, number }) => {
   const name = user.username;
   const image = user.img;
   const recent = user.recent;
   const allTime = user.alltime;
-  const activity = user.lastUpdate.slice(0,10);
+  const activity = user.lastUpdate.slice(0, 10);
 
   return (
     <tr>
       <td className="item">{number}</td>
-      <td><img src={image} className="thumbnail" alt="user thumbnail" />&nbsp;<a href={`https://www.freecodecamp.org/${name}`} rel="noreferrer noopener" target='_blank'>{name}</a></td>
+      <td>
+        <img src={image} className="thumbnail" alt="user thumbnail" />&nbsp;<a
+          href={`https://www.freecodecamp.org/${name}`}
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          {name}
+        </a>
+      </td>
       <td className="item">{recent}</td>
       <td className="item">{allTime}</td>
       <td className="item">{activity}</td>
     </tr>
   );
-}
+};
 
 export default TableListItem;
--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
-import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Header />, div);
+  ReactDOM.render(<Footer />, div);
 });
 
 test('should render Header correctly', () => {
-  const wrapper = shallow(<Header />);
+  const wrapper = shallow(<Footer />);
   expect(toJSON(wrapper)).toMatchSnapshot();
-  // expect(wrapper.find('h2').length).toBe(1);
-  // expect(wrapper.find('h2').text()).toBe("freeCodeCamp Camper Leaderboard");
 });
