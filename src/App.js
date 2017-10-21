0903bb131b13011573995efea553765c5672f48c
wildlifehexagon <crossthebreeze@gmail.com>
2017-10-20 19:51:47 -0500

    add basic tests

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import TableList from './containers/table_list';
-import Header from './components/header';
-import Footer from './components/footer';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import './style/App.css';
 
 class App extends Component {
