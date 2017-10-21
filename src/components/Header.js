247c1e7da5e23a22eadc007d0f6998655b5127df
wildlifehexagon <crossthebreeze@gmail.com>
2017-10-21 11:39:07 -0500

    add case sensitive components back

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h2>freeCodeCamp <i className="fa fa-free-code-camp fcc-icon" aria-hidden="true"></i> <br />Camper Leaderboard</h2>
+    </div>
+  )
+}
+
+export default Header;
